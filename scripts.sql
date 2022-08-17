6c3bbb5dd03237614b5c4ad92d9eb6a199d04e0c
Casey Smith <crs4f1988@gmail.com>
2022-08-16 20:40:42 -0500

    Looking at pricing

--- scripts.sql
+++ scripts.sql
@@ -1,1 +1,21 @@
--- 
+Select *
+From play_store_apps;
+
+Select *
+from play_store_apps
+Where type = 'Paid';
+
+Select name, price
+from app_store_apps
+Where price >= 5.99;
+
+Select p.name, a.price, p.install_count, p.rating
+from play_store_apps AS p
+Inner Join app_store_apps AS a
+On p.name = a.name;
+
+Select DISTINCT(p.name), a.price, p.install_count, p.rating, p.genres, p.content_rating
+from play_store_apps AS p
+Inner Join app_store_apps AS a
+On p.name = a.name;
+
