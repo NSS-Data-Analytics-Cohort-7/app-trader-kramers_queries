12eb9c0ec43891aab8d054ea404642447647893b
Casey Smith <crs4f1988@gmail.com>
2022-08-16 21:16:13 -0500

    More pricing queries

--- scripts.sql
+++ scripts.sql
@@ -19,3 +19,9 @@ from play_store_apps AS p
 Inner Join app_store_apps AS a
 On p.name = a.name;
 
+Select DISTINCT(p.name), a.price, p.install_count, p.rating, p.genres, p.content_rating
+from play_store_apps AS p
+Inner Join app_store_apps AS a
+On p.name = a.name
+WHERE a.price > '4.99';
+
