8d96e7ee090c5b5bc5e570b1cb33ef41b8c0a41d
Robert Lunn <robertrlunn@gmail.com>
2022-08-16 21:22:54 -0500

    brainstorming

---
+++ data/script.sql
@@ -0,0 +1,14 @@
+-- SELECT * 
+-- FROM app_store_apps;
+
+-- SELECT *
+-- FROM play_store_apps;
+
+-- SELECT *
+-- FROM app_store_apps AS a
+-- INNER JOIN
+-- play_store_apps AS p
+-- USING(name);
+
+
+
