a8af4ca12a7861fc903a0869f54bb7f5ed33c716
Mackeyk88 <mackeyk8@winthrop.edu>
2022-08-18 21:25:33 -0500

    Script Complete, working on final top 10

--- data/scripts.sql
+++ data/scripts.sql
@@ -8,6 +8,7 @@ SELECT DISTINCT *
 FROM app_store_apps as asa
 INNER JOIN play_store_apps as psa
 USING (name)
+WHERE asa.rating = '5.0'
 -- Apps used on both platforms 500
 
 
@@ -72,11 +73,69 @@ FROM
     INNER JOIN play_store_apps as psa
     USING (name) 
     WHERE psa.content_rating = 'Everyone') as subq
-ORDER BY net_income DESC
+ORDER BY monthly_income DESC
 LIMIT 10
 
 
 
+-- SELECT DISTINCT name, install_count, psa.review_count
+-- FROM app_store_apps as asa
+-- INNER JOIN play_store_apps as psa
+-- USING (name, review_count)
+-- ORDER BY review_count DESC
 
 
+SELECT name as app, price, genres, install_count, rating, years_survived, CAST(app_purchase_price as money),
+CAST(total_marketing_cost as money), CAST(advertising_profit as money),
+CAST((advertising_profit - (app_purchase_price + total_marketing_cost)) as money) as net_income, 
+CAST(ROUND((advertising_profit - (app_purchase_price + total_marketing_cost))/(12 * (CAST(rating as NUMERIC) * 2 + 1)),2) as money) as monthly_income
+FROM
+    (SELECT DISTINCT name, psa.install_count, asa.rating, asa.price, psa.category, psa.genres,
+    (CAST(asa.rating as NUMERIC) * 2 + 1) as years_survived,
+    CASE WHEN asa.price <= 1.00 THEN 10000  
+    WHEN asa.price > 1.00 THEN asa.price * 10000 END as app_purchase_price, 
+    (CAST(asa.rating as NUMERIC) * 2 + 1) * 1000 * 12 as total_marketing_cost, 
+    (CAST(asa.rating as NUMERIC) * 2 + 1) * 5000 * 12 as advertising_profit
+    FROM app_store_apps as asa
+    INNER JOIN play_store_apps as psa
+    USING (name) 
+    WHERE psa.content_rating = 'Everyone') as subq
+ORDER BY monthly_income DESC
+LIMIT 10
 
+
+-- Simple Query
+SELECT name as app, price, genres, install_count, rating, years_survived, CAST(app_purchase_price as money),
+CAST(total_marketing_cost as money), CAST(advertising_profit as money),
+CAST((advertising_profit - (app_purchase_price + total_marketing_cost)) as money) as net_income, 
+CAST(ROUND((advertising_profit - (app_purchase_price + total_marketing_cost))/(12 * (CAST(rating as NUMERIC) * 2 + 1)),2) as money) as monthly_income
+FROM
+    (SELECT DISTINCT name, psa.install_count, asa.rating, asa.price, psa.category, psa.genres,
+    (CAST(asa.rating as NUMERIC) * 2 + 1) as years_survived,
+    CASE WHEN asa.price <= 1.00 THEN 10000  
+    WHEN asa.price > 1.00 THEN asa.price * 10000 END as app_purchase_price, 
+    (CAST(asa.rating as NUMERIC) * 2 + 1) * 1000 * 12 as total_marketing_cost, 
+    (CAST(asa.rating as NUMERIC) * 2 + 1) * 5000 * 12 as advertising_profit
+    FROM app_store_apps as asa
+    INNER JOIN play_store_apps as psa
+    USING (name)) as subq
+ORDER BY monthly_income DESC
+
+-- CTE
+
+WITH subq AS (SELECT DISTINCT name, psa.install_count, asa.rating, asa.price, psa.category, psa.genres,
+    (CAST(asa.rating as NUMERIC) * 2 + 1) as years_survived,
+    CASE WHEN asa.price <= 1.00 THEN 10000  
+    WHEN asa.price > 1.00 THEN asa.price * 10000 END as app_purchase_price, 
+    (CAST(asa.rating as NUMERIC) * 2 + 1) * 1000 * 12 as total_marketing_cost, 
+    (CAST(asa.rating as NUMERIC) * 2 + 1) * 5000 * 12 as advertising_profit
+    FROM app_store_apps as asa
+    INNER JOIN play_store_apps as psa
+    USING (name)
+
+SELECT name as app, price, genres, install_count, rating, years_survived, CAST(app_purchase_price as money),
+CAST(total_marketing_cost as money), CAST(advertising_profit as money),
+CAST((advertising_profit - (app_purchase_price + total_marketing_cost)) as money) as net_income, 
+CAST(ROUND((advertising_profit - (app_purchase_price + total_marketing_cost))/(12 * (CAST(rating as NUMERIC) * 2 + 1)),2) as money) as monthly_income
+FROM subq
+ORDER BY monthly_income DESC
