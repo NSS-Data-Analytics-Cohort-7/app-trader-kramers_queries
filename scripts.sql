c6b46a9f0052f63f410f87cffd223199b8d76fc0
Casey Smith <crs4f1988@gmail.com>
2022-08-18 20:21:40 -0500

    Added Kevin's Query

--- scripts.sql
+++ scripts.sql
@@ -1,27 +1,26 @@
 Select *
-From play_store_apps;
+From app_store_apps;
 
-Select *
-from play_store_apps
-Where type = 'Paid';
+Select name, price, rating
+from app_store_apps;
 
-Select name, price
-from app_store_apps
-Where price >= 5.99;
+Select name, a.price, p.price, rating
+From app_store_apps AS A
+Inner JOIN play_store_apps ON
 
-Select p.name, a.price, p.install_count, p.rating
-from play_store_apps AS p
-Inner Join app_store_apps AS a
-On p.name = a.name;
-
-Select DISTINCT(p.name), a.price, p.install_count, p.rating, p.genres, p.content_rating
-from play_store_apps AS p
-Inner Join app_store_apps AS a
-On p.name = a.name;
-
-Select DISTINCT(p.name), a.price, p.install_count, p.rating, p.genres, p.content_rating
-from play_store_apps AS p
-Inner Join app_store_apps AS a
-On p.name = a.name
-WHERE a.price > '4.99';
-
+SELECT name as app, price, genres, install_count, rating, years_survived, CAST(app_purchase_price as money),
+CAST(total_marketing_cost as money), CAST(advertising_profit as money),
+CAST((advertising_profit - (app_purchase_price + total_marketing_cost)) as money) as net_income,
+CAST(ROUND((advertising_profit - (app_purchase_price + total_marketing_cost))/(12 * (CAST(rating as NUMERIC) * 2 + 1)),2) as money) as monthly_income
+FROM (SELECT DISTINCT name, psa.install_count, asa.rating, asa.price, psa.category, psa.genres,
+    (CAST(asa.rating as NUMERIC) * 2 + 1) as years_survived,
+    CASE WHEN asa.price <= 1.00 THEN 10000
+    WHEN asa.price > 1.00 THEN asa.price * 10000 END as app_purchase_price,
+    (CAST(asa.rating as NUMERIC) * 2 + 1) * 1000 * 12 as total_marketing_cost,
+    (CAST(asa.rating as NUMERIC) * 2 + 1) * 5000 * 12 as advertising_profit
+    FROM app_store_apps as asa
+    INNER JOIN play_store_apps as psa
+    USING (name)
+    WHERE psa.content_rating = 'Everyone') as subq
+ORDER BY monthly_income DESC
+LIMIT 10
